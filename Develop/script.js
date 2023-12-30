2653d189a49c1ad2464dca23ffb2eef90994b9a3
Alex Baumgartner <aabaumgartner@gmail.com>
2023-12-30 10:05:58 -0800

    almost there, need to fix button issue

--- Develop/script.js
+++ Develop/script.js
@@ -2,48 +2,105 @@
 var generateBtn = document.querySelector("#generate");
 
 //Prompt the user for password criteria
-function generatePassword() {
+function generatePassword(){
   //Prompts the user for password length 8 < 128
   var passwordLength = prompt ("type length of password: 8 - 128 characters");
   console.log(passwordLength);
   
   //Prompts the user to include lowercase, uppercase, numeric, special characters
   if (passwordLength >= 8 && passwordLength <= 128) {
-    var lowerCase = prompt("Do you want to include lowercase letters? Type y for yes, or n for no.");
+    var lowerCase = confirm("Do you want to include lowercase letters?");
     console.log(lowerCase);
   } else {
     alert("Please retry and use numbers 8-128 only.");
+    return;
   }
 
-  if (lowerCase === "y" || "n") {
-    var upperCase = prompt ("Do you want to include uppercase letters? Type y for yes, or n for no.");
+  var upperCase = confirm ("Do you want to include uppercase letters?");
     console.log(upperCase);
-  } else{
-    alert("Please retry and y for yes, or n for no only.");
-  }
 
-  if (upperCase === "y" || "n") {
-    var numeric = prompt ("Do you want to include numeric character types? Type y for yes, or n for no.");
+  var numeric = confirm ("Do you want to include numeric character types?");
     console.log(numeric);
-  } else {
-    ("Please retry and y for yes, or n for no only.");
-  }
 
-  if (numeric === "y" || "n") {
-    var specCharacters = prompt ("Do you want to include special character types (!, $, %, etc.)? Type y for yes, or n for no.");
+  var specCharacters = confirm ("Do you want to include special character types (!, $, %, etc.)?");
     console.log(specCharacters);
-  } else {
-    ("Please retry and y for yes, or n for no only.");
-  }
 
-  //Generates password 
-  
+
+  //possible characters for categories
+  var lettersLower = "abcdefghijklmnopqrstuvwxyz";
+  var lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var symbols = "!@#$%^&*()-+~{}[]<>?";
   
   //Validates input
-  
+  if (lowerCase === true){
+    lettersLower = "abcdefghijklmnopqrstuvwxyz"; 
+  }
+  else{
+    lettersLower = "";
+  }
+  //check lowercase letters
+  console.log(lettersLower)
 
+  if (upperCase === true){
+    lettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+  }
+  else{
+    lettersUpper = "";
+  }
+  //check uppercase letters
+  console.log(lettersUpper)
+
+  if (numeric === true){
+    numbers = "0123456789"; 
+  }
+  else{
+    numbers = "";
+  }
+  //check numbers letters
+  console.log(numbers)
+
+  if (specCharacters === true){
+    symbols = "!@#$%^&*()-+~{}[]<>?"; 
+  }
+  else{
+    symbols = "";
+  }
+  //check symbols
+  console.log(symbols)
+
+  //variable for all characters - only includes category if true from confirm window(s)
+  var allCharacters = lettersLower + lettersUpper + numbers + symbols;
+  
+  //Generates password 
+  var generatedRandomPassword = "";
+  while(passwordLength - 0 > generatedRandomPassword.length){
+    generatedRandomPassword += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+  }
+
+  console.log(generatedRandomPassword)
+
+  
+    
+  //   if (lowerCase === true){
+  //     allCharacters = lettersLower;
+  //   }
+  //   else if (upperCase === true){
+  //     allCharacters = lettersLower + lettersUpper;
+  //   }
+  //   if (numeric === false) {
+  //     allCharacters = lettersLower + lettersUpper + symbols;
+  //   }
+  //   if (specCharacters === false) {
+  //     allCharacters = lettersLower + lettersUpper + numbers;
+  //   }
+  // }
+  // console.log(generatedRandomPassword)
+  
+  
   //Password is displayed on page
-  return "";
+  return generatedRandomPassword;
+
 }
 
 // Write password to the #password input
